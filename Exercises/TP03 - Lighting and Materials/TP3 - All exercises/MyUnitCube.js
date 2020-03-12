5014594acace88a66935f39a85ebfaf7ee10e20b
Samuel <diogosamuelfernandes@outlook.com>
2020-03-12 22:32:11 +0000

    Starting week 3

---
+++ Exercises/TP03 - Lighting and Materials/TP3 - All exercises/MyUnitCube.js
@@ -0,0 +1,139 @@
+/**
+ * MyUnitCube
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyUnitCube extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+      0.5, 0.5, 0.5,
+      -0.5, 0.5, 0.5,
+      -0.5, -0.5, 0.5,
+			0.5, -0.5, 0.5,
+      0.5, 0.5, -0.5,
+      -0.5, 0.5, -0.5,
+      -0.5, -0.5, -0.5,
+			0.5, -0.5, -0.5,
+
+			0.5, 0.5, 0.5,
+			-0.5, 0.5, 0.5,
+			-0.5, -0.5, 0.5,
+			0.5, -0.5, 0.5,
+			0.5, 0.5, -0.5,
+			-0.5, 0.5, -0.5,
+			-0.5, -0.5, -0.5,
+			0.5, -0.5, -0.5,
+
+			0.5, 0.5, 0.5,
+			-0.5, 0.5, 0.5,
+			-0.5, -0.5, 0.5,
+			0.5, -0.5, 0.5,
+			0.5, 0.5, -0.5,
+			-0.5, 0.5, -0.5,
+			-0.5, -0.5, -0.5,
+			0.5, -0.5, -0.5,
+
+			0.5, 0.5, 0.5,
+			-0.5, 0.5, 0.5,
+			-0.5, -0.5, 0.5,
+			0.5, -0.5, 0.5,
+			0.5, 0.5, -0.5,
+			-0.5, 0.5, -0.5,
+			-0.5, -0.5, -0.5,
+			0.5, -0.5, -0.5,
+
+			0.5, 0.5, 0.5,
+			-0.5, 0.5, 0.5,
+			-0.5, -0.5, 0.5,
+			0.5, -0.5, 0.5,
+			0.5, 0.5, -0.5,
+			-0.5, 0.5, -0.5,
+			-0.5, -0.5, -0.5,
+			0.5, -0.5, -0.5,
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+      //Face 0
+			0, 1, 2,
+      2, 3, 0,
+
+      //Face 2
+      0, 4, 5,
+      5, 1, 0,
+
+      //Face 3
+      5,6,1,
+      6,2,1,
+
+      //Face 4
+      2,6,7,
+      2,7,3,
+
+      //Face 5
+      0,3,7,
+      4,0,7,
+
+      //Face 6
+      5,4,7,
+      5,7,6
+		];
+
+
+
+		this.normals = [];
+		for (var i = 0; i < 4; i++) {
+				this.normals.push(0, 0, 1);
+		}
+		for (var i = 0; i < 4; i++) {
+				this.normals.push(0, 0, -1);
+		}
+		this.normals.push(1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(1, 0, 0);
+
+		this.normals.push(1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(1, 0, 0);
+
+		this.normals.push(1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(1, 0, 0);
+
+		this.normals.push(1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(-1, 0, 0);
+		this.normals.push(1, 0, 0);
+
+		this.normals.push(0, 1, 0);
+		this.normals.push(0, 1, 0);
+		this.normals.push(0, -1, 0);
+		this.normals.push(0, -1, 0);
+
+		this.normals.push(0, 1, 0);
+		this.normals.push(0, 1, 0);
+		this.normals.push(0, -1, 0);
+		this.normals.push(0, -1, 0);
+
+		/*
+		for (var i = 0; i < 4; i++) {
+				this.normals.push(1, 0, 0);
+		}
+		for (var i = 0; i < 4; i++) {
+				this.normals.push(0, -1, 0);
+		}
+		for (var i = 0; i < 4; i++) {
+				this.normals.push(0, 1, 0);
+		}
+*/
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	}
+}
